6426445b03c673d9654e27b3ceecdd197bbe537a
yzqdev <yzqdev@outlook.com>
2021-08-01 16:43:16 +0800

    更新依赖

--- app/src/main/java/com/javiersantos/mlmanager/activities/AboutActivity.java
+++ app/src/main/java/com/javiersantos/mlmanager/activities/AboutActivity.java
@@ -3,12 +3,10 @@ package com.javiersantos.mlmanager.activities;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.javiersantos.mlmanager.MLManagerApplication;
 import com.javiersantos.mlmanager.R;
